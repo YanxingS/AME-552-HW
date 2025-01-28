509d07e29034be2f41aae377f991873454e30dc7
YanxingS <yanxings@usc.edu>
2025-01-27 21:10:51 -0800

    hw update

--- HW1Problem3.m
+++ HW1Problem3.m
@@ -7,4 +7,13 @@ A_eq2 = [3 6; 0 1];
 A_eq3 = [1 0; 2 2];
 A_eq4 = [-3 2; 1 1];
 
-[V,D] = eig(A_eq4)
+[V,D] = eig(A_eq4);
+
+% Non linear system equations
+
+odefun = @(t,x)[x(1)*(3-x(1)-2*x(2));x(2)*(2-x(1)-x(2))];
+plotpp(odefun,'xlim',[0 10],'ylim',[0 10])
+
+ylabel('x(2)')
+xlabel('x(1)')
+title('phase portrait of nonlinear system problem 3')
